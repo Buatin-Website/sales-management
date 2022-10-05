3af45c8b09bcc79f2f93875c099bd239241d8d60
Andreas Asatera <tolong@buatin.website>
2022-10-06 05:30:09 +0700

    chore: generate resource permission & install filament spotlight

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -3,72 +3,142 @@
 namespace App\Policies;
 
 use App\Models\User;
+
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class UserPolicy
 {
     use HandlesAuthorization;
 
-    public function viewAny(User $user): bool
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
     {
-        if (! $user->isSuperAdmin()) {
-            return false;
-        }
-
-        return true;
+        return $user->can('view_any_user');
     }
 
-    public function view(User $user): bool
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user)
     {
-        if (! $user->isSuperAdmin()) {
-            return false;
-        }
-
-        return true;
+        return $user->can('view_user');
     }
 
-    public function create(User $user): bool
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
     {
-        if (! $user->isSuperAdmin()) {
-            return false;
-        }
-
-        return true;
+        return $user->can('create_user');
     }
 
-    public function update(User $user): bool
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user)
     {
-        if (! $user->isSuperAdmin()) {
-            return false;
-        }
-
-        return true;
+        return $user->can('update_user');
     }
 
-    public function delete(User $user): bool
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user)
     {
-        if (! $user->isSuperAdmin()) {
-            return false;
-        }
-
-        return true;
+        return $user->can('delete_user');
     }
 
-    public function restore(User $user): bool
+    /**
+     * Determine whether the user can bulk delete.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function deleteAny(User $user)
     {
-        if (! $user->isSuperAdmin()) {
-            return false;
-        }
-
-        return true;
+        return $user->can('delete_any_user');
     }
 
-    public function forceDelete(User $user): bool
+    /**
+     * Determine whether the user can permanently delete.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user)
     {
-        if (! $user->isSuperAdmin()) {
-            return false;
-        }
+        return $user->can('force_delete_user');
+    }
 
-        return true;
+    /**
+     * Determine whether the user can permanently bulk delete.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDeleteAny(User $user)
+    {
+        return $user->can('force_delete_any_user');
+    }
+
+    /**
+     * Determine whether the user can restore.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user)
+    {
+        return $user->can('restore_user');
+    }
+
+    /**
+     * Determine whether the user can bulk restore.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restoreAny(User $user)
+    {
+        return $user->can('restore_any_user');
+    }
+
+    /**
+     * Determine whether the user can bulk restore.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function replicate(User $user)
+    {
+        return $user->can('replicate_user');
+    }
+
+    /**
+     * Determine whether the user can reorder.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function reorder(User $user)
+    {
+        return $user->can('reorder_user');
     }
 }
